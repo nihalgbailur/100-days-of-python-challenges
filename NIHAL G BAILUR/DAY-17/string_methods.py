10f6ff46a6f808ef564a8bc922a25b683829999d
NIHAL G BAILUR <nihalgbailur@NIHALs-MacBook-Air.local>
2025-06-04 10:18:15 +0530

    string method

---
+++ NIHAL G BAILUR/DAY-17/string_methods.py
@@ -0,0 +1,14 @@
+# checking type of python
+name ="hello"
+print(type(name))
+
+
+
+# methods used in strings upper,lower,captalize,count
+name_1 ="hello"
+print(name_1.upper())
+
+
+name_2 ="hello"
+print(name_2.count('l')) 
+# argument should be passed ,if it is empty it will throw error
